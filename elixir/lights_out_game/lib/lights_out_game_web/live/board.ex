fe7825ecd7c5fdd9f6ad6ded14cee9f03d883df4
Sherry Guo <sherryguo@guopengsmac.local>
2023-10-15 22:59:55 -0700

    lights out game: also select adjacent tiles

--- elixir/lights_out_game/lib/lights_out_game_web/live/board.ex
+++ elixir/lights_out_game/lib/lights_out_game_web/live/board.ex
@@ -2,14 +2,15 @@ defmodule LightsOutGameWeb.Board do
   # `use` calls the live_view function in web module
   use LightsOutGameWeb, :live_view
 
+  @type coordinates :: {integer(), integer()}
+  @type tile :: {coordinates(), Boolean.t()}
+
   @doc """
   Setup the 5x5 grid.
   Also look at this list comprehension!
   """
   def mount(_params, _session, socket) do
     grid = for x <- 0..4, y <- 0..4, into: %{}, do: {{x, y}, false}
-
-    IO.inspect(grid)
 
     {:ok, assign(socket, grid: grid)}
   end
@@ -21,15 +22,30 @@ defmodule LightsOutGameWeb.Board do
     grid_x = String.to_integer(strX)
     grid_y = String.to_integer(strY)
 
-    IO.inspect("x: #{grid_x}, y: #{grid_y}")
+    updated_tiles =
+      {grid_x, grid_y}
+      |> with_adjacent_tiles()
+      |> Enum.map(fn coords -> flip_tile(coords, grid) end)
+      |> Enum.into(%{})
 
-    # flip grid button color
-    updated_grid = Map.put(grid, {grid_x, grid_y}, !grid[{grid_x, grid_y}])
+    updated_grid = Map.merge(grid, updated_tiles)
 
-    IO.inspect("reversed tile: #{!grid[{grid_x, grid_y}]}")
-    IO.inspect(updated_grid, label: "updated grid")
-
-    # reassign updated socket
     {:noreply, assign(socket, grid: updated_grid)}
+  end
+
+  @spec with_adjacent_tiles(coordinates()) :: list(coordinates())
+  def with_adjacent_tiles({x, y}) do
+    prevX = Kernel.max(0, x - 1)
+    nextX = Kernel.min(4, x + 1)
+
+    prevY = Kernel.max(0, y - 1)
+    nextY = Kernel.min(4, y + 1)
+
+    [{x, y}, {x, prevY}, {x, nextY}, {prevX, y}, {nextX, y}]
+  end
+
+  @spec flip_tile(coordinates(), map()) :: tile()
+  def flip_tile({x, y}, grid) do
+    {{x, y}, !grid[{x, y}]}
   end
 end
